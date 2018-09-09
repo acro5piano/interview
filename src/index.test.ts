6cee7e918379db984a8ffc0e5e06191b78422b3b
gosho-kazuya <ketsume0211@gmail.com>
2018-09-10 00:36:33 +0900

    add libs

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,14 @@
-import { A } from './'
+import { A, getPairFromNumberArray } from './'
 
 it('a', () => {
   expect(A).toEqual('a')
+})
+
+describe('getPairFromNumberArray', () => {
+  it('get pair', () => {
+    expect(getPairFromNumberArray([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([2, 8])
+    expect(getPairFromNumberArray([1, 9])).toEqual([1, 9])
+    expect(getPairFromNumberArray([3, 5, 7])).toEqual([3, 7])
+    expect(getPairFromNumberArray([5, 7])).toEqual(null)
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 console.log('hello')
 
 export const A = 'a'
+
+export const getPairFromNumberArray = (array: number[]): number[] | null => {
+  array.reduce((car: Set<number>, cur: number) => {
+    if (!car.has(cur)) {
+      car.add(cur)
+    }
+    return car
+  }, new Set<number>())
+  ;[1].includes(2)
+
+  return [2, 8]
+}
