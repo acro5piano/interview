2876b1b94b174ac8ffa9d0335f457529828fdeeb
gosho-kazuya <ketsume0211@gmail.com>
2018-09-10 19:10:45 +0900

    implement

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,23 @@
-import { A, getPairFromNumberArray } from './'
+import { A, getPairFromNumberArray, getPairFromNumberArrayUsingFor } from './'
 
 it('a', () => {
   expect(A).toEqual('a')
 })
 
+describe('getPairFromNumberArrayUsingFor', () => {
+  it('get pair', () => {
+    expect(getPairFromNumberArrayUsingFor([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([1, 9])
+    expect(getPairFromNumberArrayUsingFor([1, 9])).toEqual([1, 9])
+    expect(getPairFromNumberArrayUsingFor([3, 5, 7])).toEqual([3, 7])
+    expect(getPairFromNumberArrayUsingFor([5, 7])).toEqual(null)
+  })
+})
+
 describe('getPairFromNumberArray', () => {
   it('get pair', () => {
-    expect(getPairFromNumberArray([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([2, 8])
-    expect(getPairFromNumberArray([1, 9])).toEqual([1, 9])
-    expect(getPairFromNumberArray([3, 5, 7])).toEqual([3, 7])
+    expect(getPairFromNumberArray([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([6, 4])
+    expect(getPairFromNumberArray([1, 9])).toEqual([9, 1])
+    expect(getPairFromNumberArray([3, 5, 7])).toEqual([7, 3])
     expect(getPairFromNumberArray([5, 7])).toEqual(null)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,46 @@ console.log('hello')
 
 export const A = 'a'
 
+// hash map pattern, o(n)
 export const getPairFromNumberArray = (array: number[]): number[] | null => {
-  array.reduce((car: Set<number>, cur: number) => {
-    if (!car.has(cur)) {
-      car.add(cur)
+  const res = array.reduce((car: Set<number> | number[], cur: number) => {
+    if (car instanceof Array) {
+      return car
     }
-    return car
-  }, new Set<number>())
-  ;[1].includes(2)
 
-  return [2, 8]
+    if (car.has(10 - cur)) {
+      return [cur, 10 - cur]
+    }
+
+    return car.add(cur)
+  }, new Set<number>())
+  if (res instanceof Array) {
+    return res
+  }
+  return null
+
+  // const set = new Set<number>()
+  //
+  // for (const element of array) {
+  //   if (set.has(10 - element)) {
+  //     return [element, 10 - element]
+  //   } else {
+  //     set.add(element)
+  //   }
+  // }
+  //
+  // return null
+}
+
+// for pattern, o(n^2)
+export const getPairFromNumberArrayUsingFor = (array: number[]): number[] | null => {
+  for (const [i, element1] of array.entries()) {
+    for (const element2 of array.slice(i + 1)) {
+      if (element1 + element2 === 10) {
+        return [element1, element2]
+      }
+    }
+  }
+
+  return null
 }
