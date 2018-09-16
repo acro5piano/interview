00f3de1c1baeb6bea21ed76d1ab6b32d8dc8e828
gosho-kazuya <ketsume0211@gmail.com>
2018-09-16 13:58:35 +0900

    change prettier config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function meetingPlanner(slotsA, slotsB, dur) {
+  // your code goes here
+}
+
+const res = meetingPlanner(
+  [[10, 50], [60, 120], [140, 210]],
+  [[0, 15], [60, 70]],
+  9,
+)
+console.log(res)
