02974ac2162419bbf4535623c6f2e4684bceca73
gosho-kazuya <ketsume0211@gmail.com>
2018-09-16 13:59:25 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 function meetingPlanner(slotsA, slotsB, dur) {
-  // your code goes here
+  const res = []
+  for (const a of slotsA) {
+    for (const b of slotsB) {
+      if (b[0] <= a[0] && a[0] + dur <= b[1]) {
+        return [a[0], a[0] + dur]
+      }
+    }
+  }
+
+  return null
 }
 
 const res = meetingPlanner(
   [[10, 50], [60, 120], [140, 210]],
   [[0, 15], [60, 70]],
-  9,
+  8,
 )
 console.log(res)
