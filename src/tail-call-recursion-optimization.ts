84869c47b72d32302a436a53458afc486e871b2b
gosho-kazuya <ketsume0211@gmail.com>
2018-09-17 17:34:11 +0900

    tail call recursion optimizationg

--- src/tail-call-recursion-optimization.ts
+++ src/tail-call-recursion-optimization.ts
@@ -6,22 +6,33 @@ export function factorial(n: number): number {
   }
 }
 
-export function factorialTailCall(n: number): number {
-  function _factorial(n: number, acc: number): number {
-    if (n === 0) {
-      return acc
-    } else {
-      return _factorial(n - 1, n * acc)
-    }
+export function factorialTailCall(n: number): Promise<number> {
+  function _factorial(n: number, acc: number): Promise<number> {
+    return new Promise(resolve => {
+      if (n === 0) {
+        return resolve(acc)
+      } else {
+        return process.nextTick(() => {
+          resolve(_factorial(n - 1, n * acc))
+        })
+      }
+    })
   }
-
   return _factorial(n, 1)
 }
 
 const range = (r: number) => Array(r).fill(0)
+factorialTailCall(10000).then(console.log)
 
-range(10).forEach(() => {
-  range(10000).forEach((_, index) => {
-    factorialTailCall(index)
+range(100).forEach(() => {
+  range(1000).forEach((_, index) => {
+    factorialTailCall(index).then(console.log)
   })
 })
+
+// range(10).forEach(() =>
+//   range(10000).forEach(async (_, index) => {
+//     const res = await factorialTailCall(index)
+//     console.log(res)
+//   }),
+// )
